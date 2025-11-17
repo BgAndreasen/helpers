4f081fc6f5a8002d5eff0c75a62bea4a56168658
BgAndreasen <bir_and@hotmail.com>
2025-11-17 15:44:34 +0100

    version issue when installing

--- src/helpers/__init__.py
+++ src/helpers/__init__.py
@@ -1,5 +1,7 @@
 from .importers import ROMSReader
 from .visualisers import farcoast_animation
+#from .ladimparquet import ladim_auto_to_parquet
+#from .parquets import load_rls_particles, ladim_nc_to_parquet_with_rls
 
 # Version of helper package
 __version__ = "0.0.1"
